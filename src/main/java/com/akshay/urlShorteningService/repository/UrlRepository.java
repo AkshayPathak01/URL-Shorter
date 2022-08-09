e90b36a4ef133e5dbd5088f7fe4193cf0896006b
Akshaypathak01 <akshaypathak1520@gmail.com>
2022-08-09 13:12:46 +0530

    first commit

---
+++ src/main/java/com/akshay/urlShorteningService/repository/UrlRepository.java
@@ -0,0 +1,11 @@
+package com.akshay.urlShorteningService.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.akshay.urlShorteningService.model.Url;
+
+@Repository
+public interface UrlRepository extends JpaRepository<Url,Long>
+{
+    public Url findByShortLink(String shortLink);
+}
